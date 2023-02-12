e37b516ff3264a21c8a58f2d7c4ac4a55fe36871
Michal Koczkodon <github@mihau.co>
2023-02-12 14:12:09 +0100

    stuff to set up gh-page

---
+++ createGHPageDir.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+async function createGHPageDir() {
+  if (fs.existsSync('./ghpage')) {
+    await fs.promises.rm('./ghpage', {recursive: true});
+  }
+  
+  await fs.promises.mkdir('./ghpage');
+  await fs.promises.cp('./index.html', './ghpage/index.html');
+  await fs.promises.cp('./static', './ghpage/static', {recursive: true});
+  await fs.promises.cp('./dist', './ghpage/dist', {recursive: true});
+  await fs.promises.appendFileSync('./ghpage/CNAME', 'wybory.lol', {flag: 'w'});
+}
+
+createGHPageDir();
--- src/_config.prod.js
+++ src/_config.prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  reqUrl: '/'
+  reqUrl: 'https://raw.githubusercontent.com/sedes-dev/wybory-lol/main/data/newest.json'
 }
